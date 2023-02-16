96b444622d9a00cb1a8283ffe45f2c9a09b9701a
RafinurM <zadorinka16@mail.ru>
2023-02-17 05:42:24 +0700

    resolve task

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Link,
+  NavLink,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Home from "./components/Home";
+import New from "./components/New";
+import PostView from "./components/PostView";
+import EditPost from "./components/EditPost";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Router>
+          <div className="header">
+            <Link to="/" className="title">
+              Social club
+            </Link>
+            <NavLink to="/posts/new" className={"create-post-button"}>
+              Create post
+            </NavLink>
+          </div>
+
+          <Routes>
+            <Route index path="/" element={<Home />} />
+            <Route path="/posts/new" element={<New />} />
+            <Route path="/posts/:id" element={<PostView />} />
+            <Route path='/posts/:id/edit' element={<EditPost />} />
+          </Routes>
+        </Router>
+    </>
   );
 }
 
